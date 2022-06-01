addb9f16f40ede41e1e32782c97f4f983f900dfc
freguesy <freguesy@gmail.com>
2022-06-01 10:12:23 -0300

    33

--- IMC.py
+++ IMC.py
@@ -3,7 +3,7 @@ from tkinter import *
 #   'criar janela'
 
 root = Tk()
-root.grid_rowconfigure(0, weight=0)
+root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
 #   'Max e Min da Janela'
@@ -22,12 +22,12 @@ def imc():
 
 #   'Criar os Widgets'
 
-lb1 = Label(root, text='Altura')
-in1 = Entry(root, font="Arial 26")
-lb2 = Label(root, text='Peso')
-in2 = Entry(root, font="Arial 26")
-bt1 = Button(root, text='IMC', command=imc)
-lb3 = Label(root, text= "Status", font="Arial 16")
+lb1 = Label(root, text='Altura',background='#7505D6',foreground='white')
+in1 = Entry(root, font='algerian 24',background='black',foreground='white')
+lb2 = Label(root, text='Peso',background='#7505D6',foreground='white')
+in2 = Entry(root, font='algerian 24',background='black',foreground='white')
+bt1 = Button(root, text='IMC', command=imc, background='#7505D6',activebackground='#D62626',foreground='white')
+lb3 = Label(root, text= "Status", font='algerian 24',background='black',foreground='white')
 
 #   'Organizar os Widgets'
 
