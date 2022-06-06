b616c7efe1e9e2cdd51ef07e83c5eb1a2e81da9b
freguesy <freguesy@gmail.com>
2022-06-06 09:20:49 -0300

    1

--- calculadora completa.py
+++ calculadora completa.py
@@ -6,10 +6,21 @@ from tkinter import *
 def entrada(valor):
     lb1['text'] += valor
 
+
+def limpar():
+    lb1["text"] = ""
+
+def r ():
+    x=eval(lb1['text'])
+    lb1['text'] = str(x)
+
+
 #   'criar grades e colunas'
 
 root = Tk()
-root.grid_rowconfigure(0, weight=0)
+root.configure(bg='red')
+root.title("calculadora")
+root.grid_rowconfigure(0, weight=1)
 root.grid_rowconfigure(1, weight=1)
 root.grid_rowconfigure(2, weight=1)
 root.grid_rowconfigure(3, weight=1)
@@ -22,35 +33,40 @@ root.grid_columnconfigure(4, weight=1)
 
 
 #   'geometria'
+
 root.geometry('220x400')
 
+
 #   'janela.config(bg='')'
+
 root.minsize(width=300, height=100)
 root.maxsize(width=600, height=600)
 
 
 #   'criar widgets'
 
-lb1 = Label (root, text='', font='CASTELLAR')
-bt1 = Button(root, text='1', font='CASTELLAR', command=lambda: entrada('1'))
-bt2 = Button(root, text='2', font='CASTELLAR', command=lambda: entrada('2'))
-bt3 = Button(root, text='3', font='CASTELLAR', command=lambda: entrada('3'))
-bt4 = Button(root, text='4', font='CASTELLAR', command=lambda: entrada('4'))
-bt5 = Button(root, text='5', font='CASTELLAR', command=lambda: entrada('5'))
-bt6 = Button(root, text='6', font='CASTELLAR', command=lambda: entrada('6'))
-bt7 = Button(root, text='7', font='CASTELLAR', command=lambda: entrada('7'))
-bt8 = Button(root, text='8', font='CASTELLAR', command=lambda: entrada('8'))
-bt9 = Button(root, text='9', font='CASTELLAR', command=lambda: entrada('9'))
-bt10 = Button(root, text='0', font='CASTELLAR', command=lambda: entrada('0'))
-bt11 = Button(root, text='+', font='CASTELLAR', command=lambda: entrada('+'))
-bt12 = Button(root, text='-', font='CASTELLAR', command=lambda: entrada('-'))
-bt13 = Button(root, text='*', font='CASTELLAR', command=lambda: entrada('*'))
-bt14 = Button(root, text='/', font='CASTELLAR', command=lambda: entrada('/'))
-bt15 = Button(root, text='C', font='CASTELLAR', command=lambda: entrada('C'))
-bt16 = Button(root, text='=', font='CASTELLAR', command=lambda: entrada('='))
+lb1 = Label (root, text='', font='CASTELLAR',background='#5B5B69')
+bt1 = Button(root, text='1', font='CASTELLAR', command=lambda: entrada('1'),background='#4B4B4F')
+bt2 = Button(root, text='2', font='CASTELLAR', command=lambda: entrada('2'),background='#4B4B4F')
+bt3 = Button(root, text='3', font='CASTELLAR', command=lambda: entrada('3'),background='#4B4B4F')
+bt4 = Button(root, text='4', font='CASTELLAR', command=lambda: entrada('4'),background='#4B4B4F')
+bt5 = Button(root, text='5', font='CASTELLAR', command=lambda: entrada('5'),background='#4B4B4F')
+bt6 = Button(root, text='6', font='CASTELLAR', command=lambda: entrada('6'),background='#4B4B4F')
+bt7 = Button(root, text='7', font='CASTELLAR', command=lambda: entrada('7'),background='#4B4B4F')
+bt8 = Button(root, text='8', font='CASTELLAR', command=lambda: entrada('8'),background='#4B4B4F')
+bt9 = Button(root, text='9', font='CASTELLAR', command=lambda: entrada('9'),background='#4B4B4F')
+bt10 = Button(root, text='0', font='CASTELLAR', command=lambda: entrada('0'),background='#4B4B4F')
+bt11 = Button(root, text='+', font='CASTELLAR', command=lambda: entrada('+'),background='#4B4B4F')
+bt12 = Button(root, text='-', font='CASTELLAR', command=lambda: entrada('-'),background='#4B4B4F')
+bt13 = Button(root, text='x', font='CASTELLAR', command=lambda: entrada('*'),background='#4B4B4F')
+bt14 = Button(root, text='÷', font='CASTELLAR', command=lambda: entrada('/'),background='#4B4B4F')
+bt15 = Button(root, text='C', font='CASTELLAR', command=limpar,background='#4B4B4F')
+bt16 = Button(root, text='=', font='CASTELLAR',command= lambda: r(),background='#4B4B4F')
+
 
 #   'organizar widgets'
 
+lb1.grid(row=0, column=0, columnspan= 5)
 lb1.grid(row=0, column=1, sticky=NSEW)
 bt1.grid(row=1, column=1, sticky=NSEW)
 bt2.grid(row=1, column=2, sticky=NSEW)
@@ -68,7 +84,6 @@ bt13.grid(row=3, column=4, sticky=NSEW)
 bt14.grid(row=4, column=4, sticky=NSEW)
 bt15.grid(row=4, column=1, sticky=NSEW)
 bt16.grid(row=4, column=3, sticky=NSEW)
-
 
 
 #   'run'
