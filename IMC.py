471e0159f4094861960948cbe0b8bf960e76f92d
freguesy <freguesy@gmail.com>
2022-06-06 10:43:30 -0300

    frames adicionados

--- IMC.py
+++ IMC.py
@@ -1,10 +1,14 @@
+import fractions
 from tkinter import *
 
 #   'criar janela'
 
 root = Tk()
-root.grid_rowconfigure(0, weight=5)
-root.grid_columnconfigure(0, weight=5)
+
+
+fr1 = Frame(root, bg='#7505D6')
+fr2 = Frame(root, bg='#7505D6')
+fr3 = Frame(root, bg='#7505D6')
 
 #   'Max e Min da Janela'
 
@@ -22,24 +26,30 @@ def imc():
 
 #   'Criar os Widgets'
 
-lb1 = Label(root, text='Altura',background='#7505D6',foreground='white', font='algerian 24')
-in1 = Entry(root, font='algerian 24',background='black',foreground='white')
-lb2 = Label(root, text='Peso',background='#7505D6',foreground='white', font='algerian 24')
-in2 = Entry(root, font='algerian 24',background='black',foreground='white')
-bt1 = Button(root, text='IMC', command=imc, background='#7505D6',activebackground='#D62626',foreground='white', font='algerian 24')
-lb3 = Label(root, text= "Status", font='algerian 24',background='black',foreground='white')
+lb1_fr1 = Label(fr1, text='Altura',background='#7505D6',foreground='white', font='algerian 24')
+in1 = Entry(fr1, font='algerian 24',background='black',foreground='white')
+lb2_fr2 = Label(fr2, text='Peso',background='#7505D6',foreground='white', font='algerian 24')
+in2 = Entry(fr2, font='algerian 24',background='black',foreground='white')
+bt1 = Button(fr3, text='IMC', command=imc, background='#7505D6',activebackground='#D62626',foreground='white', font='algerian 24')
+lb3_fr3 = Label(fr3, text= "Status", font='algerian 24',background='black',foreground='white')
+
+
+fr1.pack()
+fr2.pack()
+fr3.pack()
+
+
 
 #   'Organizar os Widgets'
 
-lb1.grid(row=0, column=0, sticky=NSEW)
-in1.grid(row=1, column=0, sticky=NSEW)
+lb1_fr1.grid(row=0, column=0)
+in1.grid(row=1, column=0)
 
-lb2.grid(row=2, column=0, sticky=NSEW)
-in2.grid(row=3, column=0, sticky=NSEW)
+lb2_fr2.grid(row=0, column=0)
+in2.grid(row=1, column=0)
 
-bt1.grid(row=4, column=0, sticky=NSEW)
-
-lb3.grid(row=5, column=0, sticky=NSEW)
+bt1.grid(row=0, column=0)
+lb3_fr3.grid(row=1, column=0)
 
 
 #   'Run'
